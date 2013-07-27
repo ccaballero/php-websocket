326910ebc37a1cd99df458af0e3c59cb3bc8f058
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2013-07-27 15:09:28 -0400

    indentation

--- server/lib/WebSocket/Application/Application.php
+++ server/lib/WebSocket/Application/Application.php
@@ -4,66 +4,48 @@ namespace WebSocket\Application;
 
 /**
  * WebSocket Server Application
- * 
+ *
  * @author Nico Kaiser <nico@kaiser.me>
  */
 abstract class Application
 {
     protected static $instances = array();
-    
+
     /**
-     * Singleton 
+     * Singleton
      */
     protected function __construct() { }
-
     final private function __clone() { }
-    
-    final public static function getInstance()
-    {
+
+    final public static function getInstance() {
         $calledClassName = get_called_class();
         if (!isset(self::$instances[$calledClassName])) {
             self::$instances[$calledClassName] = new $calledClassName();
         }
-
         return self::$instances[$calledClassName];
     }
 
     abstract public function onConnect($connection);
+    abstract public function onDisconnect($connection);
+    abstract public function onData($data, $client);
 
-	abstract public function onDisconnect($connection);
+    // Common methods:
+    protected function _decodeData($data) {
+        $decodedData = json_decode($data, true);
+        if ($decodedData === null) {
+            return false;
+        }
+        if (isset($decodedData['action'], $decodedData['data']) === false) {
+            return false;
+        }
+        return $decodedData;
+    }
 
-	abstract public function onData($data, $client);
-
-	// Common methods:
-	
-	protected function _decodeData($data)
-	{
-		$decodedData = json_decode($data, true);
-		if($decodedData === null)
-		{
-			return false;
-		}
-		
-		if(isset($decodedData['action'], $decodedData['data']) === false)
-		{
-			return false;
-		}
-		
-		return $decodedData;
-	}
-	
-	protected function _encodeData($action, $data)
-	{
-		if(empty($action))
-		{
-			return false;
-		}
-		
-		$payload = array(
-			'action' => $action,
-			'data' => $data
-		);
-		
-		return json_encode($payload);
-	}
+    protected function _encodeData($action, $data) {
+        if (empty($action)) {
+            return false;
+        }
+        $payload = array('action' => $action, 'data' => $data);
+        return json_encode($payload);
+    }
 }
